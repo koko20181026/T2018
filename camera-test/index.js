3404ab699be9bc7d0ae0cb3ceb5ec50d33b9b78a
koko20181026 <44491209+koko20181026@users.noreply.github.com>
2018-11-01 16:13:31 +0900

    Update index.js

--- camera-test/index.js
+++ camera-test/index.js
@@ -3,9 +3,13 @@ const medias = {audio : false, video : {
           exact : "environment"
         }
       }},
-      video  = document.getElementById("video");
+      video  = document.getElementById("video"),
+      canvas = document.getElementById("canvas"),
+      ctx    = canvas.getContext("2d");
 
 navigator.getUserMedia(medias, successCallback, errorCallback);
+
+requestAnimationFrame(draw);
 
 function successCallback(stream) {
   video.srcObject = stream;
@@ -14,3 +18,11 @@ function successCallback(stream) {
 function errorCallback(err) {
   alert(err);
 };
+
+function draw() {
+  canvas.width  = window.innerWidth;
+  canvas.height = window.innerHeight;
+  ctx.drawImage(video, 0, 0);
+
+  requestAnimationFrame(draw);
+}
