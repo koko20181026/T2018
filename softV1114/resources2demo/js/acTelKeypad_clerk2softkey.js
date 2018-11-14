6e56e21045b730f00ac29672389f8c21d8ddaa47
koko20181026 <44491209+koko20181026@users.noreply.github.com>
2018-11-14 12:01:34 +0900

    Add files via upload

---
+++ softV1114/resources2demo/js/acTelKeypad_clerk2softkey.js
@@ -0,0 +1,76 @@
+$(document).ready(function() {
+    var offsettop;
+    var ScrTop;
+    
+    $("#acTelKeypad_clerk2").load("acTelKeypad_clerk2.html");
+    
+    $("input[type=text]").mouseup(function(key) {
+
+        offsettop = $(key.target).offset().top;
+
+        $('html, body').animate({scrollTop : offsettop}, 400);
+
+        if ($("#acTelKeypad_clerk2").css("display") == "none") {
+            $('#acTelKeypad_clerk2').css("top", offsettop + 1000);
+            $('#acTelKeypad_clerk2').fadeIn({queue : false,duration : 500});
+            $('#acTelKeypad_clerk2').animate({top : offsettop + 50}, 500);
+
+            var acTelKeypad_clerk2Height = $("#acTelKeypad_clerk2").height();
+            ScrTop = $(document).scrollTop();            
+            var dummyT = $("<table></table>").css("border","0").css("height",acTelKeypad_clerk2Height);
+            $("#dummyT").append(dummyT);
+        }
+        
+        $("#nameKeypad2").keypad({
+            target : $(key.target),
+            clearText : '全削除',
+            backText : '1文字削除',
+            spacebarText : 'ｽﾍﾟｰｽ',
+            switchText : '英数字選択',
+            layout : kanaLayout02,
+            switchLayout : qwertyLayout02,
+            onKeypress : keypad_OnKeypress,
+            keypadOnly : false
+        });
+    });
+
+    $(document).on("click touchstart","#acTelKeypad_clerk2close",function(){
+        if ($("#acTelKeypad_clerk2").css("display") != "none") {
+            $('#acTelKeypad_clerk2').fadeOut({queue : false,duration : 500});
+            $('#acTelKeypad_clerk2').animate({top : offsettop + 600}, 500);
+
+            $('html, body').animate({scrollTop : ScrTop}, 500);
+            $("#dummyT").empty();
+        }
+    });
+
+    $("input").keydown(function(key) {
+        if (key.keyCode == 13 && ($("#acTelKeypad_clerk2").css("display") != "none")) {
+                $('#acTelKeypad_clerk2').fadeOut({queue : false,duration : 500});
+                $('#acTelKeypad_clerk2').animate({top : offsettop + 600}, 500);
+
+                $('html, body').animate({scrollTop : ScrTop}, 500);
+                $("#dummyT").empty();
+        }
+    });
+
+    $("input[type=text]").mouseup(function(key) {
+        $("#nameKeypad2").keypad("option", {layout: kanaLayout02, target:$(key.target)});
+    });
+    
+    $(".physickey").mousedown(function(key) {
+         $("input[type=text]").each(function(i,e){
+            if(e.readOnly == false){
+                e.readOnly = true;
+                $(".physickey").css("background","green");
+            }else{
+                e.readOnly = false;
+                $(".physickey").css("background","gray");
+            }
+         });
+    });
+});
+
+function keypad_OnKeypress() {
+  $("#nameKeypad2").keypad("option", "target").focus();
+}
