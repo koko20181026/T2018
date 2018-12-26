b63b8698f4b7efc160be05474c9f8b2aa6e7a8e6
koko20181026 <44491209+koko20181026@users.noreply.github.com>
2018-12-26 14:25:05 +0900

    Update modalsoftkey.js

--- whitesoftkeyV3/resources2demo/js/modalsoftkey.js
+++ whitesoftkeyV3/resources2demo/js/modalsoftkey.js
@@ -44,7 +44,7 @@ $(document).ready(function() {
     $('input[type=text]').blur(function(){
 
     	$('input[type=text]').attr('readonly',true);
-        $(this).css("background-color","white");
+//         $(this).css("background-color","white");
     });
 
     $(document).on("click touchstart","#modalclose",function(){
